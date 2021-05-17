568a3b08be3febe9c145a22883a44fbce050f196
greg <gabriel.gregorio1987@gmail.com>
2021-05-17 11:32:07 -0300

    criando rota de listagem de categorias

---
+++ module/Category/categoryController.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const Categories = require('./Categories');
+const slugify = require('slugify');
+
+router.get("/admin/categories",(req,res) => {
+    Categories.findAll().then((categories) => {
+        res.render("admin/categories/index", {
+            categories : categories
+        })
+    })
+})
