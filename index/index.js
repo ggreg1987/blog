6fcca2d81cf46edcfd53e1bb2f1fdc1118671f18
greg <gabriel.gregorio1987@gmail.com>
2021-05-17 09:55:59 -0300

    configurando o express-session

--- index/index.js
+++ index/index.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 app.set('view engine','ejs');
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(session({secret:"hardSecrety", cookie : {maxAge : 36000000}}))
 
 app.listen(9090,() => {
     console.log("O servidor subiu na porta 9090")
