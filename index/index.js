cfd66769fc5499a53b796b560fdcd940eb206dd5
greg <gabriel.gregorio1987@gmail.com>
2021-05-17 09:45:48 -0300

    definindo a pasta public como estática

--- index/index.js
+++ index/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 
 app.set('view engine','ejs');
+app.use(express.static('public'))
 
 app.listen(9090,() => {
     console.log("O servidor subiu na porta 9090")
