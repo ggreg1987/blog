a473a6b848e4a128fc6728919ede5146ce0a90bc
greg <gabriel.gregorio1987@gmail.com>
2021-05-17 10:29:22 -0300

    Criando a rota de salvar usuario

--- module/userController.js
+++ module/userController.js
@@ -2,11 +2,34 @@ const express = require('express');
 const router = express.Router();
 const User = require('./User');
 const bcrypt = require('bcryptjs');
-const slugify = require('slugify');
+
 
 
 router.get("/admin/user/create",(req,res) => {
     res.render("admin/user/create")
 })
 
+router.post("/usersave", (req,res) => {
+    let username = req.body.username;
+    let password = req.body.password;
 
+    User.findOne({where: {username : username}}).then((user) => {
+        if(user == undefined) {
+
+            let salt = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(password,salt);
+            let correctPassword = bcrypt.compareSync(user.password,hash)
+
+            if(correctPassword) {
+                User.create({
+                    username : username,
+                    password : hash
+                }).then(() => {
+                    res.redirect("/admin/category")
+                })
+            } else {
+                res.redirect("/amin/user/create")
+            }
+        }
+    })
+})
