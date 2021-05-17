c74935e87bc5abbf70c64ac85acfe2ed335bb2ac
greg <gabriel.gregorio1987@gmail.com>
2021-05-17 10:45:12 -0300

    importando o userController

--- index/index.js
+++ index/index.js
@@ -3,7 +3,8 @@ const app = express()
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const connection = require('../database/database');
-const User = require('../module/User');
+const User = require('../module/User/User');
+const userController = require('../module/User/userController');
 
 connection
     .authenticate()
