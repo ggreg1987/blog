49e6c5ce82a809d4a6194d1ecb11e4c5b3b5400e
greg <gabriel.gregorio1987@gmail.com>
2021-05-17 09:43:52 -0300

    configurando a view engine e criando uma pasta views

--- index/index.js
+++ index/index.js
@@ -3,6 +3,8 @@ const app = express()
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
+app.set('view engine','ejs');
+
 app.listen(9090,() => {
     console.log("O servidor subiu na porta 9090")
 })
