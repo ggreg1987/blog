30b4ec5189e370b803fa15cf024002a45f791997
greg <gabriel.gregorio1987@gmail.com>
2021-05-17 10:06:18 -0300

    importando o modulo de usuario

--- index/index.js
+++ index/index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const connection = require('../database/database');
+const User = require('../module/User');
 
 connection
     .authenticate()
