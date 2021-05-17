fcd0635fd0f4ae1b7cda7f636f0a4bb919534fd8
greg <gabriel.gregorio1987@gmail.com>
2021-05-17 09:42:16 -0300

    Criando um servidor na porta 9090

--- index/index.js
+++ index/index.js
@@ -2,3 +2,7 @@ const express = require('express');
 const app = express()
 const bodyParser = require('body-parser');
 const session = require('express-session');
+
+app.listen(9090,() => {
+    console.log("O servidor subiu na porta 9090")
+})
