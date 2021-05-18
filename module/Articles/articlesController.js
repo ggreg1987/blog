d483410980d61cb665e055a729bab0f941b02e37
greg <gabriel.gregorio1987@gmail.com>
2021-05-18 11:46:26 -0300

    Listar todos os artigos e join com categoria

--- module/Articles/articlesController.js
+++ module/Articles/articlesController.js
@@ -3,3 +3,13 @@ const router = express.Router()
 const Article = require('./Articles');
 const Categories = require('../Category/Categories');
 const slugify = require('slugify');
+
+router.get("/admin/articles",(req,res) => {
+    Article.findAll({
+        include : [{model:Categories}]
+    }).then((articles) => {
+        res.render("admin/articles/index",{
+            articles : articles
+        })
+    })
+})
